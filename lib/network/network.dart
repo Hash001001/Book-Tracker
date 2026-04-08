faf678e5310cbd4b78492c4f4b604ee7f2743d57
muhammad.asher <Asher@321>
2026-04-08 13:47:21 +0500

    - Network library added - implemented network class and added seachBook method

---
+++ lib/network/network.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:flutter_book_reader/models/book.dart';
+import 'package:http/http.dart' as http;
+
+class Network {
+  static const _baseUrl = "https://openlibrary.org/search.json";
+
+  Future<List<Docs>> searchBooks(String query) async {
+    var url = Uri.parse("$_baseUrl?q=$query");
+    var response = await http.get(url);
+    List<Docs> docs = [];
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      if (data["docs"] != null && data["docs"] is List) {
+        docs = (data["docs"] as List)
+            .map((bookItem) => Docs.fromJson(bookItem as Map<String, dynamic>))
+            .toList();
+      }
+    } else {
+      print("Error occurred while fetching data");
+    }
+    return docs;
+  }
+}
