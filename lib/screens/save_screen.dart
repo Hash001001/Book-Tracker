1f21ff2a7d6a674895543711f9364157a46f761c
muhammad.asher <Asher@321>
2026-04-13 12:46:10 +0500

    fetching and showing book data in saved screen

--- lib/screens/save_screen.dart
+++ lib/screens/save_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_book_reader/database/datebase_helper.dart';
+import 'package:flutter_book_reader/models/book.dart';
 
 class SavedScreen extends StatefulWidget {
   const SavedScreen({super.key});
@@ -11,8 +13,20 @@ class _SavedScreenState extends State<SavedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Saved"),
+      body: FutureBuilder(
+        future: DataBaseHelper.instance.getAllBooks(),
+        builder: (context, snapshot) =>
+           snapshot.hasData ? ListView.builder(
+            itemCount: snapshot.data?.length,
+            itemBuilder: (context, index) {
+              Docs bookData = snapshot.data![index];
+
+              return ListTile(title: Text(bookData.title));
+            },
+          ) : Center(
+            child: CircularProgressIndicator(),
+          )
+        ,
       ),
     );
   }
